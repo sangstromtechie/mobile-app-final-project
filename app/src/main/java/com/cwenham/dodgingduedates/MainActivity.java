1d5374fd0a982719b69b65778735212d5a647451
Unknown <cwenham@gmail.com>
2018-04-13 12:15:19 -0500

    Added AdMob to app

--- app/src/main/java/com/cwenham/dodgingduedates/MainActivity.java
+++ app/src/main/java/com/cwenham/dodgingduedates/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private GameView gameView;
     private Handler handler = new Handler();
     private final static long TIMER_INTERVAL = 30;
+    private InterstitialAd interstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,18 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }, 0, TIMER_INTERVAL);
+
+        //Display Ad when a user first launches the app.
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+        interstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdLoaded() {
+                //Ad finished loading.
+                interstitialAd.show();
+            }
+        });
     }
 
     @Override
